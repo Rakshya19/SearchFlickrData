6ba8e1326f77db7a6961237f03d430f6416f54cb
unknown <me.rakshya18@gmail.com>
2019-08-27 21:35:44 +1000

    view search flickr image committed

--- app.js
+++ app.js
@@ -1,34 +1,134 @@
 
 (function(){
     'use strict';
-    angular.module('flickrapp',['ngMaterial'])
-    .controller('ListController',['$scope','$http',function($scope,$http){
+    var app=angular.module('flickrapp',['ngMaterial','kendo.directives','ui.bootstrap'])
+    .controller('ListController',['$scope','$http','$uibModal',function($scope,$http,$uibModal){
         $scope.results=[];
         $scope.isSearching=false;
+        $scope.searchImage='';
 
         $scope.search=function(){
-
             $scope.isSearching=true;
-            $http({
-                method: 'GET',
-                url: 'https://api.flicker.com/services/rest',function (res) {
-                res.header("Access-Control-Allow-Origin", "*");
-                res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+           myDataSource.page(1);
+        }
+
+        //KendoGrid Start
+
+        var myDataSource = new kendo.data.DataSource({
+            serverPaging: true,
+            serverSorting: true,
+            pageSize: 100,
+
+            type:'json',
+            requestStart: function () {
+            },
+            transport: {
+                read: function (options) {
+                    $scope.isSearching=true;
+                    $http({
+                        method: 'GET',
+
+                        url: 'https://www.flickr.com/services/rest',
+                        contentType:'application/x-www-form-urlencoded',
+                        params: {
+                            method: $scope.searchImage==''?'flickr.photos.getRecent':'flickr.photos.search',
+                            api_key: '35ab4be9e6966d365fb7a3248aa7330b',
+                            format:'json',
+                            nojsoncallback:1,
+                           // auth_token:'72157710563907618-cbe92558ef6c3ee5',
+                           // api_sig: '3ec0f338293ff243cf3a4259a7ca054c',
+                            tags: $scope.searchImage,
+                            per_page: myDataSource.pageSize(),
+                            page: myDataSource.page()
+                        }
+                    }).then(function (response) {
+                            options.success(response.data.photos);
+                            $scope.isSearching=false;
+
+                        },
+                        function (err) { });
+                },
+            },
+
+            schema: {
+                data: "photo",
+                total: "total"
 
             },
-                contentType:'application/x-www-form-urlencoded',
-                params: {
-                    method: 'flickr.photos.search',
-                    api_key: '479b4fbe5d60749623c3edd6584f53fa',
-                    text: $scope.searchImage,
-                    format:'json',
-                    nojsoncallback:1
+        });
 
+        $scope.myGridOption = {
+            dataSource: myDataSource,
+
+            pageable: {
+                pageSizes: [10, 25, 50, 100],
+                refresh: true,
+                change: function (e) { }
+            },
+            sortable: {
+                allowUnsort: true
+            },
+            noRecords: {
+                template: "No items to display."
+            },
+            height: 550,
+            resizable: true,
+            scrollable: true,
+            columns: [
+                {
+                    field: "View",
+                    title: "Action",
+                    sortable: false,
+                    template:
+                        '<button type="button" class="k-button" data-button-id="\'#= id #\'" ng-click="ViewImage(this.dataItem)" class="btn btn-default btn-xs"><i class="fa fa-edit"></i> <span>View</span> </button>',
+                }, {
+                    field: "id",
+                    hidden:true
+                },
+                {
+                    field: "title",
+                    title: "Title of Image"
+
+                }, {
+                    field: "farm",
+                    title: 'Farm'
+                },
+                {
+                    field: "server",
+                    title: 'Server'
+                },
+                {
+                    field: "secret",
+                    title: 'Secret'
+                },{
+                    field:"Photo",
+                    title:"Photo",
+                    template:'<img ng-src="https://farm#: farm #.staticflickr.com/#: server #/#: id #_#:secret #_b.jpg" alt="" style="width: 40px">'
                 }
-        }).then(function(response){
-                $scope.results=response;
-                $scope.isSearching=false;
-            })
+            ],
+            //rowTemplate: '<tr data-uid="#= uid #" data-id="#= ContractNumber #"> <td>#:Advertiser #</td></tr>',
+
+            dataBound: function (e) {
+                // triggers when all data is binded to the grid
+            },
+            dataBinding: function (e) { //triggers before grid binds data
+
+            },
+        };
+        $scope.ViewImage=function (data) {
+
+         var modalInstance =  $uibModal.open({
+            templateUrl: "modelTemplate.html",
+             scope: $scope
+         });
+         $scope.imagedata=data;
+        $scope.imagesource='https://farm'+data.farm+'.staticflickr.com/'+data.server+"/"+data.id+"_"+data.secret+"_b.jpg" ;
+
+            $scope.ok = function(){
+                modalInstance.close("Ok");
+            }
         }
+
     }])
 })();
